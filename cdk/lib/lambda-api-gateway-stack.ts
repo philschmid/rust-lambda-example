e2a99fcab90386ce6521bb017df3928af9798336
philschmid <schmidphilipp1995@gmail.com>
2023-03-09 23:30:26 +0100

    changed to http api

--- cdk/lib/lambda-api-gateway-stack.ts
+++ cdk/lib/lambda-api-gateway-stack.ts
@@ -2,8 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { RustFunction } from 'cargo-lambda-cdk';
 import * as path from 'path';
-import { RestApi, LambdaIntegration, Cors } from 'aws-cdk-lib/aws-apigateway';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Architecture } from 'aws-cdk-lib/aws-lambda';
+import { CorsHttpMethod, HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,17 +16,37 @@ export class InfrastructureStack extends cdk.Stack {
     });
 
     // Create an API Gateway resource for each of the CRUD operations
-    const api = new RestApi(this, 'api', {
-      restApiName: 'Rust API',
-
+    const httpApi = new HttpApi(this, 'HttpApi', {
+      corsPreflight: {
+        allowHeaders: ['*'],
+        allowMethods: [
+          CorsHttpMethod.POST,
+        ],
+        allowOrigins: ['*'],
+      },
     });
 
-    // adds /api to the API Gateway
-    const routes = api.root.addResource('api');
-    // add /test routes for lambda with CORS
-    const trackUsage = routes.addResource('test');
-    trackUsage.addCorsPreflight({ allowOrigins: ["*"], allowMethods: ["POST"], allowHeaders: ["*"], allowCredentials: true });
-    trackUsage.addMethod('POST', new LambdaIntegration(fn));
+    const LambdaIntegration = new HttpLambdaIntegration('LambdaIntegration', fn);
+    httpApi.addRoutes({
+      path: '/test',
+      methods: [HttpMethod.POST],
+      integration: LambdaIntegration,
+    });
+
+    // create cloudformation output for the API Gateway URL
+    new cdk.CfnOutput(this, 'API URL', {
+      value: httpApi.url!,
+    }
+    )
+
+    // // adds /api to the API Gateway
+    // const routes = api.root.addResource('api');
+    // // add /test routes for lambda with CORS
+    // const trackUsage = routes.addResource('test');
+    // trackUsage.addCorsPreflight({ allowOrigins: ["*"], allowMethods: ["POST"], allowHeaders: ["*"], allowCredentials: true });
+    // trackUsage.addMethod('POST', new LambdaIntegration(fn));
+
+
 
   }
 }
